22c430a41f13106dc9418e9cc613355ead80e341
Str1nder <rm552238@fiap.com.br>
2023-08-03 21:22:36 -0300

    aula 1 - revisão

---
+++ rm552238/js/script.js
@@ -0,0 +1,119 @@
+// // console.log("Hello World!");
+
+// // //Declarando um array de alunos
+// // var alunos = ["João", "Maria", "José"];
+// // //Imprimir um array de alunos
+// // console.log(alunos);
+// // //Imprimir a primeira posição do array de alunos
+// // console.log(alunos[0]);
+
+// // //Declarar um array de array de alunos
+// // var alunos = [
+// //     ["João", 8, 7.5, 8.5],
+// //     ["Maria", 9, 6, 8.5],
+// //     ["José", 8, 7, 8]
+// // ];
+// // //Imprimir um array de array de alunos
+// // console.log(alunos);
+
+// // //Imprimir a primeira posição do array de array de alunos
+// // console.log(alunos[0]);
+
+// //Declar um array de objetos
+// let alunos = [
+//     {nome : "João", nota: 8, disciplina: "Português"},
+//     {nome : "Maria", nota: 9, disciplina: "Matemática"},
+//     {nome : "José", nota: 8, disciplina: "História"}
+// ];
+// //Imprimir um array de objetos
+// console.log(alunos);
+
+// //Imprimir a primeira posição do array de objetos e suas propiedades
+// console.log(alunos[0].nome, alunos[0].nota, alunos[0].disciplina);
+// console.log(alunos[0]["nome"], alunos[0]["nota"], alunos[0]["disciplina"] );
+
+// //Quebrando linha com \n
+// console.log("Quebrando linha com \\n");
+// console.log("\n");
+
+// //Imprimir array de objetos e suas propiedades utilizando forEach e template string
+// alunos.forEach(aluno => {
+//     console.log(`${aluno.nome} - ${aluno.nota} - ${aluno.disciplina}`);
+// })
+
+//Declarando um array de objjetos e suas propriedades
+// let alunos = [
+//     {nome : "João", nota: 8, disciplina: "Português"},
+//     {nome : "Maria", nota: 9, disciplina: "Matemática"},
+//     {nome : "José", nota: 8, disciplina: "História"}
+// ];
+
+// //imprimmir array de objetos no forEach
+// alunos.forEach(aluno => {
+//     console.log(aluno);
+// })
+
+// //Quebrar linha
+// console.log("\n");
+
+// //utilizando o método PUSH para adicionar um novo objeto ao array de objetos
+// alunos.push({nome : "Pedro", nota: 10, disciplina: "Geografia"});
+
+// //imprimmir array de objetos no forEach
+// alunos.forEach(aluno => {
+//     console.log(aluno);
+// })
+
+// //Quebrar linha
+// console.log("\n");
+
+//Criando array de nomes de alunos
+let alunos = ["João", "Maria", "José"];
+console.log(alunos);
+//Aplicando o metodo sort para ordenar o array de nomes de alunos
+alunos.sort();
+//Imprimir array de nomes de alunos
+console.log(alunos);
+
+//Invertendo a ordem do array com reverse
+alunos.reverse();
+//Imprimir array de nomes de alunos
+console.log(alunos);
+//Removendo o último elemento do array com pop
+alunos.pop();
+//Imprimir array de nomes de alunos
+console.log(alunos);
+
+//Adicionando um novo elemento ao array com unshift
+alunos.unshift("Pedro");
+//Imprimir array de nomes de alunos
+console.log(alunos);
+
+//Removendo um elemento do array com shift
+alunos.shift();
+//Imprimir array de nomes de alunos
+console.log(alunos);
+
+//Adicionando um novo elemento ao array com unshift
+alunos.unshift("Pedro");
+
+//Alterar elementos do array com splice
+alunos.splice(1, 1, "Joaquina");
+
+//Imprimir array de nomes de alunos
+console.log(alunos);
+
+//Alterar elementos do array com splice
+alunos.splice(1, 2, "Maria", "Jorge");
+
+//Imprimir array de nomes de alunos
+console.log(alunos);
+
+//Remover elementos do array com splice
+alunos.splice(2, 2);
+
+//Imprimir array de nomes de alunos
+console.log(alunos);
+
+
+
