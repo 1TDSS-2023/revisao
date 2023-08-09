b4afb6a699ad9ef3309c6e613b314eaa6460bf63
kkoliveiraa <RM550815@fiap.com.br>
2023-08-09 20:08:14 -0300

    metodos map e filter no js

--- rm550815/js/script.js
+++ rm550815/js/script.js
@@ -43,24 +43,79 @@
 
 
 //Declarando um array de objetos e suas propriedades
-let alunos = [
-    {nome: "João", nota: 8, disciplina: "Português"},
-    {nome: "Maria", nota: 9, disciplina: "Matemática"},
-    {nome: "José", nota: 8, disciplina: "História"}
-];
-//Imprimir um array de objetos no forEach
-alunos.forEach(aluno => {
-    console.log(aluno);
-});
-//Quebrar linha
-console.log("\n");
+// let alunos = [
+//     {nome: "João", nota: 8, disciplina: "Português"},
+//     {nome: "Maria", nota: 9, disciplina: "Matemática"},
+//     {nome: "José", nota: 8, disciplina: "História"}
+// ];
+// //Imprimir um array de objetos no forEach
+// alunos.forEach(aluno => {
+//     console.log(aluno);
+// });
+// //Quebrar linha
+// console.log("\n");
 
-//Utilizando o método PUSH adicionar um novo objeto ao array de objetos
-alunos.push({nome : "Pedro", nota: 10, disciplina: "Geografia"});
-//Imprimir um array de objetos no forEach
-alunos.forEach(aluno => {
-    console.log(aluno);
-});
-//Quebrar linha
-console.log("\n");
+// //Utilizando o método PUSH adicionar um novo objeto ao array de objetos
+// alunos.push({nome : "Pedro", nota: 10, disciplina: "Geografia"});
+// //Imprimir um array de objetos no forEach
+// alunos.forEach(aluno => {
+//     console.log(aluno);
+// });
+// //Quebrar linha
+// console.log("\n");
+
+// const cursos = [
+//     {'nome': 'HTML5', 'duracao': '3 meses'},
+//     {'nome': 'CSS3', 'duracao': '4 meses'},
+//     {'nome': 'JavaScript', 'duracao': '5 meses'}
+// ];
+
+// //Imprimir um array de objetos
+// console.log(cursos);
+
+// //Quebrar linha
+// console.log("\n");
+// //Criar um array de nomes de cursos
+// const nomeCursos = cursos.map(curso => curso.nome);
+// //Imprimir array de nomes de cursos
+// console.log(nomeCursos);
+
+// //Quebrar linha
+// console.log("\n");
+// //Criar um array de propaganda de cursos com map
+// const propCursos = cursos.map(curso => 'O curso de ' + curso.nome + ' tem duração de ' + curso.duracao);
+// //Imprimir array de propaganda de cursos com forEach
+// propCursos.forEach(propCurso => console.log(propCurso));
+// //Quebrar linha
+// console.log("\n");
+
+// //Criando uma propaganda de cursos com map e template string utilizando o indice gerado pelo map para concatenar com o nome do curso
+// const propCursos2 = cursos.map((curso, indice) => `O curso ${indice + 1} é o curso de ${curso.nome} e tem duração de ${curso.duracao}`);
+// //Imprimir array de propaganda de cursos com forEach
+// propCursos2.forEach(propCurso => console.log(propCurso));
+
+// const notasAlunos = [ 3, 5, 8, 10, 7, 9, 6, 4, 2, 1 ];
+
+// //Notas acima da média
+// const notasAcimaMedia = notasAlunos.filter(nota => nota >= 7);
+// console.log(notasAcimaMedia);
+// //Quebrar linha
+// console.log("\n");
+
+// //Notas pares
+// const notasPares = notasAlunos.filter(nota => nota % 2 == 0);
+// console.log(notasPares);
+// //Quebrar linha
+// console.log("\n");
+
+//Declaração de um array de objetos com nome de usuários e senha.
+// const usuarios = [
+//     {nome: "João", senha: 123},
+//     {nome: "Maria", senha: 456},
+//     {nome: "José", senha: 789}
+// ];
+
+// //Verificar com o método filter se o usuário e senha estão corretos
+// const logado = usuarios.filter(usuario => usuario.nome == "João" && usuario.senha == 123);
+// console.log(logado);
 
