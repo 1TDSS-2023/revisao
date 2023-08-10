a106ce08ad48aa88bc472bc7b84e2720ab1c6493
LucasSpinelli003 <rm97757@fiap.com.br>
2023-08-09 22:59:18 -0300

    add

---
+++ rm97757/js/script.js
@@ -0,0 +1,27 @@
+// const cursos = [
+//     {nome: "HTML5", duracao: "3 meses"},
+//     {nome: "CCS3", duracao:"4 meses"},
+//     {nome:"Javascript", duracao:"5 meses"}
+// ];
+
+// console.log(cursos)
+
+// console.log("\n")
+// //Cria um Array atravez de um Array já existente
+// const infoCursos = cursos.map(curso =>`O curso de ${curso.nome} possui duração de ${curso.duracao}`)
+
+// infoCursos.forEach(infoCurso => console.log(infoCurso))
+
+// console.log('\n')
+
+// const notasAlunos = [3,5,7,8,2,9,4,10,1,2,4];
+
+// const notasAcimaDaMedia = notasAlunos.filter(nota => nota>=7)
+// console.log(notasAcimaDaMedia)
+
+// const notasPares = notasAlunos.filter(nota => nota % 2 === 0)
+// console.log(`As notas pares são ${notasPares}`)
+// notasPares.forEach(notaPar => console.log(`Esse é uma nota par = ${notaPar}`))
+
+// const notasImpares = notasAlunos.filter(nota => nota % 2 !== 0)
+// console.log(`As notas impares são ${notasImpares}`)
