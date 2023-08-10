9768df6ce6c70cd6f40fe7896244332b6930e083
Esparza_Leonardo <rm550200@fiap.com.br>
2023-08-09 21:22:17 -0300

    Manipulação de arrays

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# revisao
-Revisando conceitos de manipulação de arrays e objetos de JavaScript
+# Revisando
+
+## Aqui vamos estudar a estrutura de Arrays, Objetos e manipulação do DOM juntamente com localStorage.
+- Vamos aproveitar para criar alguns exercícios e ao final realizar nosso primeiro CheckPoint que será individual em Laboratório.
+- As orientações são as de sempre, realizem os exercícios propostos.
+- Tirem suas dúvidas através do Teams.
+- Utilizem largamente o Github+VsCode pois na próxima etapa serão de grande importância em nosso desenvolvimento.
--- rm550200/js/script.js
+++ rm550200/js/script.js
@@ -19,7 +19,7 @@
 // //Imprimir a primeira posição do array de array de alunos
 // console.log(alunos[0]);
 
-//Declar um array de objetos
+// //Declar um array de objetos
 // let alunos = [
 //     {nome : "João", nota: 8, disciplina: "Português"},
 //     {nome : "Maria", nota: 9, disciplina: "Matemática"},
@@ -28,18 +28,19 @@
 // //Imprimir um array de objetos
 // console.log(alunos);
 
-// //imprimir a primeira posição do array de objetos
+// //Imprimir a primeira posição do array de objetos e suas propiedades
 // console.log(alunos[0].nome, alunos[0].nota, alunos[0].disciplina);
-// console.log(alunos[0]["nome"],alunos[0]["nota"],alunos[0]["disciplina"]);
+// console.log(alunos[0]["nome"], alunos[0]["nota"], alunos[0]["disciplina"] );
 
-// //quebra de linha comm \n
-// console.log("quebrando linha com \\n");
+// //Quebrando linha com \n
+// console.log("Quebrando linha com \\n");
 // console.log("\n");
 
-// //imprimir array de objetos e suas propriedades ultilizando foreach e template string
+// //Imprimir array de objetos e suas propiedades utilizando forEach e template string.
 // alunos.forEach(aluno => {
 //     console.log(`${aluno.nome} - ${aluno.nota} - ${aluno.disciplina}`);
 // });
+
 
 // //Declarando um array de objetos e suas propiedades
 // let alunos = [
@@ -65,56 +66,100 @@
 // //Quebrar linha
 // console.log("\n");
 
-// //aplicando ordem alfabetica com SORT
-// // console.log(alunos.sort(a,b)=> {
-    
-// // });
-// //Imprimir array de objetos no forEach
-// alunos.forEach(aluno => {
-//     //Aplicar o método sort para ordenar os nomes em ordem alfabética.
-// });
+// //Criando array de nomes de alunos
+// let alunos = ["João", "Maria", "José"];
+// console.log(alunos);
+// //Aplicando o metodo sort para ordenar o array de nomes de alunos
+// alunos.sort();
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//criando array de nomes de alunos
-let alunos = ["Joao", "Maria", "Jose"];
-console.log(alunos);
+// //Invertendo a ordem do array com reverse
+// alunos.reverse();
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//aplicando o metodo SORT
-alunos.sort();
+// //Removendo o último elemento do array com pop
+// alunos.pop();
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//imprimir nomes dps do SORT
-console.log(alunos);
+// //Adicionando um novo elemento ao array com unshift
+// alunos.unshift("Pedro");
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//inverter ordem dos nomes com REVERSE
-alunos.reverse();
-console.log(alunos);
+// //Removendo o primeiro elemento do array com shift
+// alunos.shift();
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//remomvendo ultimo nome com POP
-alunos.pop();
-console.log(alunos);
 
-//adicionando novo elemento com UNSHIFT
-alunos.unshift("Pedro");
-console.log(alunos);
+// //Adicionando um novo elemento ao array com unshift
+// alunos.unshift("Pedro");
 
-//removendo primeiro nome com SHIFT
-alunos.shift();
-console.log(alunos);
+// //Alterar elementos do array com splice
+// alunos.splice(1, 1, "Joaquina");
 
-//adicionando novo elemento com UNSHIFT
-alunos.unshift("Pedro");
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//alterar elementos com SPLICE
-alunos.splice(1, 1, "Joaquina");
-console.log(alunos);
+// //Alterar elementos do array com splice
+// alunos.splice(1, 2, "Maria","Jorge");
 
-//Alterar elementos do array com splice
-alunos.splice(1, 2, "Maria","Jorge");
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//Imprimir array de nomes de alunos
-console.log(alunos);
+// //Removendo elementos do array com splice
+// alunos.splice(2, 2);
 
-//Removendo elementos do array com splice
-alunos.splice(2, 2);
+// //Imprimir array de nomes de alunos
+// console.log(alunos);
 
-//Imprimir array de nomes de alunos
-console.log(alunos);
+// const cursos = [
+//     {nome: "HTML", duracao: "3 meses"},
+//     {nome: "CSS3", duracao: "4 meses"},
+//     {nome: "JavaScript", duracao: "5 meses"}
+// ];
+
+// //imprimir cursos e quebra de linha
+// console.log(cursos);
+// console.log("\n");
+// //criar um array de nomes  de cursos com map
+// const nomeCursos = cursos.map(curso => curso.nome);
+// //imprimir nomes dos cursos
+// console.log(nomeCursos);
+// //quebra de linha
+// console.log("\n");
+// //Criar um array de propaganda de cursos com map
+// const propCursos = cursos.map(curso => `O curso de ${curso.nome} possui duração de ${curso.duracao}`);
+// //Imprimir array de propaganda de cusos com foreach
+// propCursos.forEach(propCursos => console.log(propCursos))
+// //quebra de linha
+// console.log("\n");
+
+// //propaganda com map e template de string ultilizando indice gerado pelo map para concatenar o nome do curso
+// const propCursos2 = cursos.map((curso, indice) => `O curso ${indice + 1} - ${curso.nome} possui duração de ${curso.duracao}`);
+// //imprimir array de propaganda de cursos com forEach
+// propCursos2.forEach(propCurso => console.log(propCurso));
+
+//filter
+
+// const notasAlunos = [3, 5, 8, 10, 7, 9, 6, 4, 2, 1]
+
+// //notas acima da media
+// const notasAcimaMedia = notasAlunos.filter(nota => nota > 7);
+// console.log(notasAcimaMedia);
+// console.log("\n");
+
+// const notasPares = notasAlunos.filter(nota => nota % 2 == 0);
+// console.log(notasPares);
+// console.log("\n");
+
+//declarando um array com nomes e senhas de usuarios
+const usuarios =  [
+    {usuario: "joao", senha: "123456"}
+    {usuario: "maria", senha: "4321"}
+    {usuario: "jose", senha: "7823"}
+]
+
